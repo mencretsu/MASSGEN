6dec199e495e63b6b3c67e74b2542295007cda9f
iz <43661631+mencretsu@users.noreply.github.com>
2025-06-08 17:13:01 +0700

    Create create.py

---
+++ create.py
@@ -0,0 +1,14 @@
+from eth_account import Account
+
+list_wallet = input('Number of wallets: ')
+wallets = []
+
+for _ in range(list_wallet):
+    acct = Account.create()
+    wallets.append((acct.address, acct.key.hex()))
+
+with open("wallets.txt", "w") as f:
+    for addr, key in wallets:
+        f.write(f"{addr},{key}\n")
+
+print(f"{list_wallet} Saved -> wallets.txt")
